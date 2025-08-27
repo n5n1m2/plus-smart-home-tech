951920edbb36c4d83ae67f0a6e7bfca2ef4904ee
n5n1m2 <n5n1m2@mail.ru>
2025-08-27 08:24:39 +0300

    fix SensorEventDeserializer.java

--- telemetry/aggregator/src/main/java/aggregator/kafka/SensorEventDeserializer.java
+++ telemetry/aggregator/src/main/java/aggregator/kafka/SensorEventDeserializer.java
@@ -4,6 +4,10 @@ import org.apache.avro.Schema;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 public class SensorEventDeserializer extends AvroDeserializer<SensorEventAvro> {
+    public SensorEventDeserializer() {
+        super(SensorEventAvro.getClassSchema());
+    }
+
     public SensorEventDeserializer(Schema schema) {
         super(schema);
     }
