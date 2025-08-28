852ec9a9d05cdd1e9e6bfe164d39fd42b701086f
n5n1m2 <n5n1m2@mail.ru>
2025-08-28 03:31:53 +0300

    2025-08-28 03:31:47

--- telemetry/collector/src/main/java/collector/service/SensorEventService.java
+++ telemetry/collector/src/main/java/collector/service/SensorEventService.java
@@ -1,15 +1,12 @@
 package collector.service;
 
-import collector.model.sensors.*;
-import collector.model.sensors.base.SensorEvent;
 import lombok.RequiredArgsConstructor;
-import org.apache.avro.specific.SpecificRecord;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.telemetry.event.*;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 @Service
 @RequiredArgsConstructor
@@ -18,46 +15,7 @@ public class SensorEventService {
     @Value("${topic.sensor}")
     private String topic;
 
-    public void sendToKafka(SensorEvent sensorEvent) {
-        SensorEventAvro avro = getRecord(sensorEvent);
-        producer.send(new ProducerRecord<>(topic, avro.getId(), avro));
-    }
-
     public void sendToKafka(SensorEventAvro avro) {
         producer.send(new ProducerRecord<>(topic, avro.getId(), avro));
-    }
-
-    private SensorEventAvro getRecord(SensorEvent event) {
-        SpecificRecord record = switch (event) {
-            case ClimateSensorEvent e -> ClimateSensorAvro.newBuilder()
-                    .setCo2Level(e.getCo2Level())
-                    .setHumidity(e.getHumidity())
-                    .setTemperatureC(e.getTemperatureC())
-                    .build();
-            case LightSensorEvent e -> LightSensorAvro.newBuilder()
-                    .setLuminosity(e.getLuminosity())
-                    .setLinkQuality(e.getLinkQuality())
-                    .build();
-            case MotionSensorEvent e -> MotionSensorAvro.newBuilder()
-                    .setMotion(e.getMotion())
-                    .setLinkQuality(e.getLinkQuality())
-                    .setVoltage(e.getVoltage())
-                    .build();
-            case SmartSwitchSensorEvent e -> SwitchSensorAvro.newBuilder()
-                    .setState(e.getState())
-                    .build();
-            case TemperatureSensorEvent e -> TemperatureSensorAvro.newBuilder()
-                    .setTemperatureC(e.getTemperatureC())
-                    .setTemperatureF(e.getTemperatureF())
-                    .build();
-            default -> throw new IllegalArgumentException("Unknown SensorType " + event);
-        };
-
-        return SensorEventAvro.newBuilder()
-                .setId(event.getId())
-                .setHubId(event.getHubId())
-                .setTimestamp(event.getTimestamp().toEpochMilli())
-                .setPayload(record)
-                .build();
     }
 }
