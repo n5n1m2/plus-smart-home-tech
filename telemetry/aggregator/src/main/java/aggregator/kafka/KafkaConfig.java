80e34ae1dd35a56d194b3f7357856c2d9c63d144
n5n1m2 <n5n1m2@mail.ru>
2025-08-28 04:14:37 +0300

    2025-08-28 04:14:34

--- telemetry/aggregator/src/main/java/aggregator/kafka/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/aggregator/kafka/KafkaConfig.java
@@ -1,15 +1,11 @@
 package aggregator.kafka;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
@@ -17,28 +13,22 @@ import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import java.util.Properties;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaConfig {
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-    @Value("${kafka.group.id}")
-    private String groupId;
+
+    private final KafkaPropertiesConfigurator configurator;
 
     @Bean
     public Producer<String, SpecificRecordBase> kafkaProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
+        props.putAll(configurator.getProducer());
         return new KafkaProducer<>(props);
     }
 
     @Bean
     public Consumer<String, SensorEventAvro> kafkaConsumer() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+        props.putAll(configurator.getConsumer());
         return new KafkaConsumer<>(props);
     }
 }
