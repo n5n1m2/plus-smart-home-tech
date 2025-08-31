e5eb9c16e6963460e99863b1a8787a1d199fa051
n5n1m2 <grishkoff.volodya@yandex.ru>
2025-08-31 09:26:27 +0300

    3-Aggregator

---
+++ telemetry/aggregator/src/main/java/aggregator/service/AggregationStarter.java
@@ -0,0 +1,44 @@
+package aggregator.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+import java.time.Duration;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AggregationStarter {
+
+    private final Consumer<String, SensorEventAvro> consumer;
+    private final AggregatorService service;
+    @Value("telemetry.sensors.v1")
+    private String topic;
+
+    public void start() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            new Thread(consumer::wakeup);
+        }));
+
+        try {
+            consumer.subscribe(List.of(topic));
+            while (true) {
+                ConsumerRecords<String, SensorEventAvro> records = consumer.poll(Duration.ofMillis(1000));
+                service.handle(records);
+            }
+        } catch (WakeupException e) {
+            log.error("Wakeup exception", e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            service.shutdown();
+        }
+    }
+}
--- telemetry/collector/src/main/java/collector/service/HubService.java
+++ telemetry/collector/src/main/java/collector/service/HubService.java
@@ -1,20 +1,12 @@
 package collector.service;
 
-import collector.model.hub.DeviceAddedEvent;
-import collector.model.hub.DeviceRemoveEvent;
-import collector.model.hub.ScenarioAddedEvent;
-import collector.model.hub.ScenarioRemovedEvent;
-import collector.model.hub.base.BaseHubEvent;
 import lombok.RequiredArgsConstructor;
-import org.apache.avro.specific.SpecificRecord;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.telemetry.event.*;
-
-import java.util.List;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 
 @Service
 @RequiredArgsConstructor
@@ -22,61 +14,10 @@ public class HubService {
 
     private final Producer<String, SpecificRecordBase> producer;
 
-    @Value("${topic.hubs}")
+    @Value("${kafka.topic.hubs}")
     private String topic;
-
-    public void sendToKafka(BaseHubEvent baseHubEvent) {
-        HubEventAvro event = getRecord(baseHubEvent);
-        producer.send(new ProducerRecord<>(topic, event.getHubId(), event));
-    }
 
     public void sendToKafka(HubEventAvro event) {
         producer.send(new ProducerRecord<>(topic, event.getHubId(), event));
-    }
-
-    private HubEventAvro getRecord(BaseHubEvent hubEvent) {
-        SpecificRecord record = switch (hubEvent) {
-            case DeviceAddedEvent e -> DeviceAddedEventAvro.newBuilder()
-                    .setId(e.getId())
-                    .setType(DeviceTypeAvro.valueOf(e.getDeviceType().name()))
-                    .build();
-            case DeviceRemoveEvent e -> DeviceRemovedEventAvro.newBuilder()
-                    .setId(e.getId())
-                    .build();
-            case ScenarioAddedEvent e -> {
-                List<ScenarioConditionAvro> conditionAvro = e.getConditions().stream()
-                        .map(obj -> ScenarioConditionAvro.newBuilder()
-                                .setSensorId(obj.getSensorId())
-                                .setValue(obj.getValue())
-                                .setOperation(ConditionOperationAvro.valueOf(obj.getOperation().name()))
-                                .setType(ConditionTypeAvro.valueOf(obj.getType().name()))
-                                .build())
-                        .toList();
-                List<DeviceActionAvro> actionAvro = e.getActions().stream()
-                        .map(
-                                obj -> DeviceActionAvro.newBuilder()
-                                        .setSensorId(obj.getSensorId())
-                                        .setValue(obj.getValue())
-                                        .setType(ActionTypeAvro.valueOf(obj.getType().name()))
-                                        .build()
-                        )
-                        .toList();
-                yield ScenarioAddedEventAvro.newBuilder()
-                        .setActions(actionAvro)
-                        .setConditions(conditionAvro)
-                        .setName(e.getName())
-                        .build();
-            }
-            case ScenarioRemovedEvent e -> ScenarioRemovedEventAvro.newBuilder()
-                    .setName(e.getName())
-                    .build();
-            default -> throw new IllegalArgumentException("Unexpected value: " + hubEvent);
-
-        };
-        return HubEventAvro.newBuilder()
-                .setHubId(hubEvent.getHubId())
-                .setTimestamp(hubEvent.getTimestamp().toEpochMilli())
-                .setPayload(record)
-                .build();
     }
 }
